42587cf94c32b38a897be19d1bdf1dd5dc488287
Šarūnas Būdvytis <131205176+sbudvytis@users.noreply.github.com>
2024-02-06 13:01:26 +0100

    updated variables

--- server/src/modules/appointment/create/index.ts
+++ server/src/modules/appointment/create/index.ts
@@ -80,10 +80,12 @@ export default authenticatedProcedure
       const transporter = nodemailer.createTransport({
         host: 'smtp.hostinger.com',
         port: 465,
+        secure: true,
         auth: {
           user: process.env.EMAIL,
           pass: process.env.PASSWORD,
         },
+        authMethod: 'PLAIN',
       })
 
       try {
